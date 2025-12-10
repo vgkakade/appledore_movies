853b3ee2f02962f55e25a21bd5502135cb8be80c
Vaibhav Kakade <vk.appledore@gmail.com>
2025-12-10 15:24:08 +0000

    Added new fixture data, some tweaks to models to handle null/blank values

--- movies/models.py
+++ movies/models.py
@@ -25,7 +25,7 @@ class Language(models.Model):
 # Create your models here.
 class Movies(models.Model):
     title = models.CharField(max_length=200, null=False, blank=False)
-    release_date = models.DateField()
+    release_date = models.DateField(null=True, blank=True)
     duration = models.PositiveIntegerField()
     genre = models.ManyToManyField(Genre, related_name="movies", blank=True)
     language = models.ManyToManyField(Language, related_name="movies")
@@ -33,7 +33,7 @@ class Movies(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     poster = models.ImageField(upload_to="posters/")
     trailer_url = models.URLField(null=True, blank=True)
-    rating = models.DecimalField(max_digits=2, decimal_places=1)
+    rating = models.DecimalField(max_digits=3, decimal_places=1, default=0.0)
     cast = models.ManyToManyField(Actor, related_name="movies", blank=True)
     added_on = models.DateTimeField(auto_now_add=True)
 
