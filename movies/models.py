911e5b3377599097679245529980942f0c57f8e6
Vk <33711959+vgkakade@users.noreply.github.com>
2026-05-08 17:14:55 +0530

    added updated_on field (#5)
    Co-authored-by: Vaibhav Kakade <vk.appledore@gmail.com>

--- movies/models.py
+++ movies/models.py
@@ -36,6 +36,7 @@ class Movies(models.Model):
     rating = models.DecimalField(max_digits=3, decimal_places=1, default=0.0)
     cast = models.ManyToManyField(Actor, related_name="movies", blank=True)
     added_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
