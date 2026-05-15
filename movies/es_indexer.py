9ebe7d8ce5cdc5243d629cdd3b63e3b44282e101
Vaibhav Kakade <vk.appledore@gmail.com>
2026-05-15 14:14:46 +0000

    extracted resuable methods, created celery task

--- movies/es_indexer.py
+++ movies/es_indexer.py
@@ -10,31 +10,46 @@ from movies.es_document import MovieDocument
 from appledore_movies.services.es_movie_serializer import serialize_movie
 
 logger = logging.getLogger(__name__)
+# es = Elasticsearch(settings.ELASTICSEARCH_HOST)
+CHUNK_SIZE = 1000
 
 
-def _generate_actions(index_name, chunk_size=1000):
+def _generate_actions(movies, index_name):
     offset = 0
     while True:
-        movies = Movies.objects.prefetch_related("language", "cast", "genre")[
-            offset : offset + chunk_size
-        ]
-        if not movies:
+        batch = movies[offset : offset + CHUNK_SIZE]
+        if not batch:
             break
 
-        for movie in movies:
+        for movie in batch:
             yield {
                 "_index": index_name,
                 "_id": movie.id,
                 "_source": serialize_movie(movie),
             }
 
-        offset += chunk_size
+        offset += CHUNK_SIZE
 
 
-def bulk_index(movies=None):
-    ALIAS = MovieDocument.Index.name
-    CHUNK_SIZE = 1000
+def bulk_index(movies, index_name):
     es = Elasticsearch(settings.ELASTICSEARCH_HOST)
+    logger.info("Indexing data now..")
+    try:
+        bulk(
+            client=es,
+            actions=_generate_actions(movies, index_name),
+            chunk_size=CHUNK_SIZE,
+        )
+        logger.info("Data indexed successfully...")
+        return True
+    except Exception as e:
+        logger.error(f"Indexing failed: {str(e)}")
+        return False
+
+
+def re_index():
+    es = Elasticsearch(settings.ELASTICSEARCH_HOST)
+    ALIAS = MovieDocument.Index.name
     try:
         old_index_name = es.indices.get_alias(name=ALIAS)
         old_index_name = list(old_index_name.keys())[0] if old_index_name else None
@@ -42,31 +57,25 @@ def bulk_index(movies=None):
         old_index_name = None
 
     logger.info(f"Existing index: {old_index_name}")
-
     new_index_name = f"{ALIAS}_{datetime.now().strftime('%Y%m%d%H%M%S')}"
-    MovieDocument.init(index=new_index_name)
-    logger.info(f"New index created: {new_index_name}")
-
-    logger.info("Indexing data now..")
-
     try:
-        bulk(
-            es,
-            _generate_actions(new_index_name),
-            chunk_size=CHUNK_SIZE,
-        )
+        MovieDocument.init(index=new_index_name)
+        logger.info(f"New index created: {new_index_name}")
 
-        logger.info("Data indexed successfully...")
+        movies = Movies.objects.prefetch_related("language", "cast", "genre")
+        result = bulk_index(movies, new_index_name)
+
+        if not result:
+            raise Exception("Bulk indexing failed")
 
         if old_index_name:
-            es.indices.update_aliases(
-                {
-                    "actions": [
-                        {"remove": {"index": old_index_name, "alias": ALIAS}},
-                        {"add": {"index": new_index_name, "alias": ALIAS}},
-                    ]
-                }
-            )
+            body = {
+                "actions": [
+                    {"remove": {"index": old_index_name, "alias": ALIAS}},
+                    {"add": {"index": new_index_name, "alias": ALIAS}},
+                ]
+            }
+            es.indices.update_aliases(body=body)
             logger.info(f"Switched alias to new index {new_index_name}")
             es.indices.delete(index=old_index_name)
             logger.info(f"Deleted old index {old_index_name}")
@@ -75,6 +84,5 @@ def bulk_index(movies=None):
             logger.info(f"Created new index {new_index_name} with alias '{ALIAS}'")
         logger.info("Indexing Completed!")
     except Exception as e:
-        logger.error(f"Indexing failed: {str(e)}")
+        logger.error(f"Reindex failed: {str(e)}")
         es.indices.delete(index=new_index_name, ignore=404)
-        logger.error(f"Cleaned up new index {new_index_name} due to failure")
--- movies/sync_products.py
+++ movies/sync_products.py
@@ -1,8 +1,10 @@
 from datetime import datetime
 
 from django.core.cache import cache
-from appledore_movies.celery import app
+from .es_indexer import bulk_index
+from .es_document import MovieDocument
 
+from appledore_movies.celery import app
 from .models import Movies
 
 
@@ -13,9 +15,13 @@ def sync_products():
     # 3. Update Elasticsearch index with new/updated records
     # 4. Update last_synced in redis cache
     last_synced = cache.get("last_synced")
+    index_name = MovieDocument.Index.name
+
     if last_synced is None:
         last_synced = datetime(2000, 1, 1)
 
     updated_movies = Movies.objects.filter(updated_on__gt=last_synced).prefetch_related(
         "genres", "actors", "languages"
     )
+    bulk_index(updated_movies, index_name)
+    cache.set("last_synced", datetime.now())
